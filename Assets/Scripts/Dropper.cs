fe5c3317016eb457c32d1119d6d36a4928426390
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-05-14 18:16:22 +0100

    dropping script

--- Assets/Scripts/Dropper.cs
+++ Assets/Scripts/Dropper.cs
@@ -4,20 +4,33 @@ using UnityEngine;
 public class Dropper : MonoBehaviour
 {
     [SerializeField] float timeToWait = 3f;
+    bool hasDropped = false;
+
+    MeshRenderer myMeshRenderer;
+    Rigidbody myRigidBody;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        myMeshRenderer = GetComponent<MeshRenderer>();
+        myRigidBody = GetComponent<Rigidbody>();
+
+        myMeshRenderer.enabled = false; // Disable the mesh renderer at the start
+        myRigidBody.useGravity = false; // Disable gravity at the start
     }
 
     // Update is called once per frame
     void Update()
     {
      
-        if (Time.time > timeToWait)
+        if (Time.time > timeToWait && !hasDropped)
         {
             GetComponent<Rigidbody>().useGravity = true;
+            GetComponent<MeshRenderer>().enabled = true;
+
+            Invoke(nameof(FreezeObject), 3f);
+
+            hasDropped = true;
         }
 
 
@@ -27,5 +40,11 @@ public class Dropper : MonoBehaviour
             float elapsedTime = Time.time;
             Debug.Log("Elapsed Time: " + elapsedTime + " seconds");
         }
+    }
+    void FreezeObject()
+    {
+        myRigidBody.constraints = RigidbodyConstraints.FreezePositionY |
+                                  RigidbodyConstraints.FreezeRotationX |
+                                  RigidbodyConstraints.FreezeRotationZ;
     }
 }
