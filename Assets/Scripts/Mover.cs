cbd117b9a4c62f670a2739cb081803d9259e814f
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-05-14 15:02:37 +0100

    inputs

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -2,19 +2,20 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    [SerializeField] float xValue = 0f;
-    [SerializeField] float yValue = 0.05f;
-    [SerializeField] float zValue = 0f;
     
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
+        float xValue = Input.GetAxis("Horizontal") * 0.1f;
+        float yValue = 0f;
+        float zValue = Input.GetAxis("Vertical") * 0.1f;
         transform.Translate(xValue, yValue, zValue);
     }
 }
+
+
