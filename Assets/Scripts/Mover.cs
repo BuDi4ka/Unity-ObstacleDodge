36c6c4819f167cee9bc76182e61d072fce6b9f5f
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-05-14 16:32:56 +0100

    methods

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -6,10 +6,23 @@ public class Mover : MonoBehaviour
     
     void Start()
     {
-        
+        PrintInstruction();
     }
 
     void Update()
+    {
+        MovePlayer();
+    }
+
+    void PrintInstruction()
+    {
+        Debug.Log("Use WASD or Arrow keys to move the player around.");
+        Debug.Log("Welcome to the game!");
+        Debug.Log("Don't bump into objects");
+    }
+
+
+    void MovePlayer()
     {
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float yValue = 0f;
