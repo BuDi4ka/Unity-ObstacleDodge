c17d2f5968fd11524fcba071de8e507408891ed1
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-05-15 11:03:32 +0100

    prefabs

--- Assets/Scripts/TriggerProjectile.cs
+++ Assets/Scripts/TriggerProjectile.cs
@@ -3,15 +3,22 @@ using UnityEngine;
 public class TriggerProjectile : MonoBehaviour
 {
     [SerializeField] GameObject[] projectiles;
+    bool hasActived = false;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Player")
+        if (other.gameObject.tag == "Player" && hasActived == false)
         {
             foreach (GameObject projectile in projectiles)
             {
                 projectile.SetActive(true);
-            }    
+            }
+            hasActived = true;
         }
+    }
+
+    private void Update()
+    {
+        Debug.Log("Activated - " + hasActived);
     }
 }
